d36520964d99bbac1e3f12273aad08be01eb93fb
lapis-lazulon <matthew.gilmore18@gmail.com>
2021-12-03 15:32:28 +0000

    Day 3 part 1

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,2 +1,28 @@
+#In[]
 import numpy as np
-sub_moves = np.loadtxt("input2.txt")
+sub_moves = np.loadtxt("input2.txt", str)
+# %%
+horizontal = 0
+depth = 0
+for move in sub_moves:
+    if move[0] == 'forward':
+        horizontal += int(move[1])
+    elif move[0] == 'up':
+        depth += -int(move[1])
+    else:
+        depth += int(move[1])
+print(depth, horizontal, depth * horizontal)
+# %%
+hori2 = 0
+depth2 = 0
+aim = 0
+for move in sub_moves:
+    if move[0] == 'forward':
+        hori2 += int(move[1])
+        depth2 += aim * int(move[1])
+    elif move[0] == 'up':
+        aim += -int(move[1])
+    else:
+        aim += int(move[1])
+print(depth2, hori2, depth2 * hori2)
+# %%
