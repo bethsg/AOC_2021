4b36da4453d30d3a7a8b18e5bf482de1c8bb87a9
lapis-lazulon <matthew.gilmore18@gmail.com>
2021-12-10 07:21:34 +0000

    A little tidying

--- Day10/day10.py
+++ Day10/day10.py
@@ -3,29 +3,28 @@ import numpy as np
 chunks = np.loadtxt("input10.txt", str)
 
 #In[1]
-opens = ['[','{','(','<']
+opens = ['[', '{', '(', '<']
 closes = [']','}',')','>']
 points = [57, 1197, 3, 25137]
 dictio = {}
 dictio['x'] = 'plsnokeyerror'
-pdictio = {}
+points_dic = {}
 sum_of_points = 0
+incomplete_lines = []
+
 for x in range(0, 4):
     dictio[opens[x]] = closes[x]
-    pdictio[closes[x]] = points[x]
+    points_dic[closes[x]] = points[x]
 
-incomplete_lines = []
-for x in range(0, len(chunks)):
-    chunk = chunks[x]
+for chunk in chunks:
     openers = ['x']
-    for y in range(0, len(chunk)):
-        chr = chunk[y]
+    for chr in chunk:
         if chr in opens:
             openers = np.append(openers, chr)
         elif chr == dictio[openers[-1]]:
             openers = np.delete(openers, -1)
         else:
-            sum_of_points += pdictio[chr]
+            sum_of_points += points_dic[chr]
             break
     else:
         openers = np.flip(np.delete(openers, 0))
@@ -35,15 +34,15 @@ print("Part 1: ", sum_of_points)
 
 #In[2]
 scores = []
-newpdictio = {}
-newpoints = [2,3,1,4]
+points_part2 = [2, 3, 1, 4]
 
 for x in range(0, 4):
-    newpdictio[opens[x]] = newpoints[x]
+    points_dic[opens[x]] = points_part2[x]
+
 for line in incomplete_lines:
     score = 0
     for chr in line:
-        score = (score * 5) + newpdictio[chr]
+        score = (score * 5) + points_dic[chr]
     scores = np.append(scores, score)
 
 print("Part 2: ", np.median(np.sort(scores)))
